dbeb736e33cda61c062286e0baaa0d7cce7c5ee4
idirbfs <85136127+idirbfs@users.noreply.github.com>
2023-11-06 16:46:57 +0100

    Create bdd.sql

---
+++ bdd/bdd.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Utilisateurs (
+    ID_Utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    Nom VARCHAR(50),
+    Prenom VARCHAR(50),
+    Email VARCHAR(100),
+    MotDePasse VARCHAR(255),
+    AutresInfos TEXT
+);
+
+CREATE TABLE Achats (
+    ID_Achat INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Utilisateur INT,
+    ID_Produit INT, -- Added foreign key for Produits
+    DateAchat DATE,
+    MontantTotal DECIMAL(10, 2),
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateurs(ID_Utilisateur),
+    FOREIGN KEY (ID_Produit) REFERENCES Produits(ID_Produit) -- Foreign key for Produits
+);
+
+CREATE TABLE Produits (
+    ID_Produit INT PRIMARY KEY AUTO_INCREMENT,
+    NomProduit VARCHAR(100),
+    Description TEXT,
+    Prix DECIMAL(10, 2),
+    StockDisponible INT
+);
+
+CREATE TABLE PointsFidelite (
+    ID_Point INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Utilisateur INT,
+    NombrePoints INT,
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateurs(ID_Utilisateur)
+);
+
+CREATE TABLE HistoriquePoints (
+    ID_Historique INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Utilisateur INT,
+    NombrePoints INT,
+    TypeOperation ENUM('Gagne', 'Utilise'),
+    DateOperation DATE,
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateurs(ID_Utilisateur)
+);
