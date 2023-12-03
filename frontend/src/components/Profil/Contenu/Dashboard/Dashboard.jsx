a8558b3a48a43c5a935fdd74d298b014388d57fb
idriss-debabza <48883209+idriss-debabza@users.noreply.github.com>
2023-12-03 23:03:25 +0100

    Update Dashboard.jsx

--- frontend/src/components/Profil/Contenu/Dashboard/Dashboard.jsx
+++ frontend/src/components/Profil/Contenu/Dashboard/Dashboard.jsx
@@ -3,19 +3,32 @@ import { Grid, List, Collapse, Divider, Typography, ThemeProvider, createTheme }
 import { CreditCard, Loyalty } from '@mui/icons-material';
 import { StyledTypography, StyledList, StyledListItem, StyledListItemTextCard, StyledListItemTextPoint } from './styledDashboard';
 
-const cardPaymentProducts = [
-  { id: 1, name: 'Product 1', price: '$10.99' },
-  { id: 2, name: 'Product 2', price: '$15.99' },
-  { id: 3, name: 'Product 3', price: '$20.99' },
-  { id: 4, name: 'Product 4', price: '$25.99' },
+const Products = [
+  { id: 1, name: 'Product 1', price: '$10.99' , discount  : '20%' },
+  { id: 2, name: 'Product 2', price: '$15.99', discount  : '20%' },
+  { id: 3, name: 'Product 3', price: '$20.99' , discount  : '20%'},
+  { id: 4, name: 'Product 4', price: '$25.99', discount  : '20%' },
 ];
 
 const pointPaymentProducts = [
-  { id: 1, name: 'Product 1', price: '$10.99' },
-  { id: 2, name: 'Product 2', price: '$15.99' },
-  { id: 3, name: 'Product 3', price: '$20.99' },
-  { id: 4, name: 'Product 4', price: '$25.99' },
+  { id: 1, name: 'Product 1', price: '$10.99', discount  : '20%' },
+  { id: 2, name: 'Product 2', price: '$15.99' , discount  : '20%'},
+  { id: 3, name: 'Product 3', price: '$50.99', discount  : '20%' },
+  { id: 4, name: 'Product 4', price: '$25.99' , discount  : '20%'},
 ];
+
+const purchaseHistory = [
+  { id: 1, type: 'cardPayment', products: Products },
+  { id: 2, type: 'pointPayment', products: pointPaymentProducts },
+  { id: 3, type: 'cardPayment', products: Products },
+  { id: 4, type: 'cardPayment', products: Products },
+  { id: 5, type: 'pointPayment', products: Products },
+  // Add more purchase history entries as needed
+];
+
+const calculateTotalPrice = (products) => {
+  return products.reduce((total, product) => total + parseFloat(product.price.replace('$', '')), 0).toFixed(2);
+};
 
 
 const theme = createTheme({
@@ -33,19 +46,13 @@ const theme = createTheme({
 });
 
 const Dashboard = () => {
-  const [isCardPaymentOpen, setCardPaymentOpen] = useState(false);
-  const [isPointPaymentOpen, setPointPaymentOpen] = useState(false);
+  const [openPurchase, setOpenPurchase] = useState(null);
 
-  const toggleCardPayment = () => {
-    setCardPaymentOpen(!isCardPaymentOpen);
-    
+  const togglePurchase = (id) => {
+    setOpenPurchase((prevOpenPurchase) => (prevOpenPurchase === id ? null : id));
   };
 
-  const togglePointPayment = () => {
-    setPointPaymentOpen(!isPointPaymentOpen);
-    
-  };
-
+ 
   return (
     <ThemeProvider theme={theme}>
       <div>
@@ -54,34 +61,41 @@ const Dashboard = () => {
         </StyledTypography>
         <Divider style={{ marginBottom: '20px' }} />
 
-        <StyledTypography variant="h5" onClick={toggleCardPayment} style={{ cursor: 'pointer' }}>
-          <CreditCard /> Achat avec carte bancaire
-        </StyledTypography>
-        {isCardPaymentOpen && (
-          <StyledList>
-            {cardPaymentProducts.map((product) => (
-              <StyledListItem key={product.id}>
-                <StyledListItemTextCard primary={product.name} />
-                <StyledListItemTextCard primary={product.price} />
-              </StyledListItem>
-            ))}
-          </StyledList>
-        )}
-
-        <Divider style={{ marginBottom: '20px' }} />
-        <StyledTypography variant="h5" onClick={togglePointPayment} style={{ cursor: 'pointer' }}>
-          <Loyalty /> Achat avec points
-        </StyledTypography>
-        {isPointPaymentOpen && (
-          <StyledList>
-            {pointPaymentProducts.map((product) => (
-              <StyledListItem key={product.id}>
-                <StyledListItemTextPoint primary={product.name} />
-                <StyledListItemTextPoint primary={product.price} />
-              </StyledListItem>
-            ))}
-          </StyledList>
-        )}
+        {purchaseHistory.map((purchase) => (
+          <div key={purchase.id}>
+            <StyledTypography
+              variant="h5"
+              onClick={() => togglePurchase(purchase.id)}
+              style={{ cursor: 'pointer', color: purchase.type === 'cardPayment' ? '#2196f3' : '#f44336' }}
+            >
+              {purchase.type === 'cardPayment' ? <CreditCard /> : <Loyalty />} Achat {purchase.id}
+              <span style={{ marginLeft: '10px', fontWeight: 'normal' }}>(Total: ${calculateTotalPrice(purchase.products)})</span>
+        
+            </StyledTypography>
+            <Collapse in={openPurchase === purchase.id}>
+              <StyledList>
+                {purchase.products.map((product) => (
+                  <StyledListItem key={product.id}>
+                    {purchase.type === 'cardPayment' ? (
+                      <>
+                        <StyledListItemTextCard primary={product.name} />
+                        <StyledListItemTextCard primary={product.price} />
+                        <StyledListItemTextCard primary={product.discount} />
+                      </>
+                    ) : (
+                      <>
+                        <StyledListItemTextPoint primary={product.name} />
+                        <StyledListItemTextPoint primary={product.price} />
+                        <StyledListItemTextPoint primary={product.discount} />
+                      </>
+                    )}
+                  </StyledListItem>
+                ))}
+              </StyledList>
+            </Collapse>
+            <Divider style={{ marginBottom: '20px' }} />
+          </div>
+        ))}
       </div>
     </ThemeProvider>
   );
